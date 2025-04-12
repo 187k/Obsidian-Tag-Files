980479748c4daae884806dce73f7323decfdc6ad
qwerty <48735602+187k@users.noreply.github.com>
2025-04-12 12:08:53 +0300

    Update script-tag-obsidian.py

--- script-tag-obsidian.py
+++ script-tag-obsidian.py
@@ -1,14 +1,13 @@
 import os
 
-# Папка с вашими заметками Obsidian
-vault_path = "H:/Мой диск/obsidian vault/05 - Projects/NYC Gangs/Queens"
-tag_to_add = "#nycgang"
+vault_path = "path to folder"
+tag_to_add = "tag" # example: #itnewtag
 
 for filename in os.listdir(vault_path):
     if filename.endswith(".md"):
         file_path = os.path.join(vault_path, filename)
         with open(file_path, "r+", encoding="utf-8") as file:
             content = file.read()
-            if tag_to_add not in content:  # Чтобы не дублировать теги
+            if tag_to_add not in content: 
                 file.seek(0, 0)
                 file.write(tag_to_add + "\n" + content)
